a34a4c01a32909cd28c97932217c674979e0041a
Lateral Lateral <laterallateral@Laterals-MacBook-Pro.local>
2019-05-06 14:03:03 +0300

    adding locInject

---
+++ haulerLocInjectTests/haulerLocInjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  haulerLocInjectTests.swift
+//  haulerLocInjectTests
+//
+//  Created by Lateral Lateral on 22/03/2019.
+//  Copyright © 2019 Lateral Lateral. All rights reserved.
+//
+
+import XCTest
+@testable import haulerLocInject
+
+class haulerLocInjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
